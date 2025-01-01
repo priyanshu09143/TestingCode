6e01087a704aaa31d253c2e273aa0ce5451d12ff
priyanshu09143 <vishal9772345@gmail.com>
2025-01-01 13:51:38 +0530

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import express from "express";
+import mongoose from "mongoose";
+const app = express()
+
+mongoose.connect("mongodb://localhost:27017/test").then(() => {
+    console.log("database is Connected")
+}).catch(() => {
+    console.log('something went wrong in data base')
+})
+
+app.set("view engine", "ejs")
+
+app.use(express.json())
+app.use(express.static('public'))
+
+
+app.get("/", async (req, res) => {
+
+    let data = await axios.get("https://dummyjson.com/products")
+
+    res.render("index", { data })
+})
+
+app.listen(3000, (req, res) => {
+    console.log("server is running on port 3000")
+})
